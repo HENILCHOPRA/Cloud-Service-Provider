7fbde7a80df1aebc3ce6dd4f58489b1323f66bb2
dell <siddhantsukhatankar@gmail.com>
2021-11-18 17:43:22 -0500

    Ordered Table creation

--- Tables.sql
+++ Tables.sql
@@ -1,7 +1,32 @@
-use CCSDB;
+use cloudcomputingDB;
+
+create table paymentInformation(
+    payID varchar(20) primary key,
+    methodType varchar(20));
+
+create table authorization(
+    authID varchar(20) primary key,
+    userName varchar(20), 
+    password varchar(20));
+
+create table subscription(
+    subscriptionID varchar(20) primary key,
+    startDate date,
+    price int);
+
+create table storage(
+    storageID varchar(20) primary key,
+    size varchar(20),
+    type varchar(20));
+
+create table computing(
+    computeID varchar(20) primary key,
+    coreCount varchar(20),
+    ram varchar(20),
+    gpu varchar(20));
 
 create table user(
-	userID varchar(20) primary key, 
+    userID varchar(20) primary key, 
     name varchar(20),
     email varchar(20),
     payID varchar(20),
@@ -10,70 +35,40 @@ create table user(
     foreign key(payID) references paymentInformation (payID) on delete cascade,
     foreign key(authID) references authorization (authID) on delete cascade);
 
-
-drop table user;
-drop table billing;
-
 create table billing(
-	bilID varchar(20) primary key,
+    bilID varchar(20) primary key,
     amount int,
     billDate date,
     dueDate date,
     subscriptionID varchar(20),
     foreign key(subscriptionID) references subscription(subscriptionID)on delete cascade);
     
-create table subscription(
-	subscriptionID varchar(20),
-    startDate date,
-    price int);
-    
-create table authorization(
-	authID varchar(20) primary key,
-	userName varchar(20), 
-	password varchar(20));
-        
-create table paymentInformation(
-	payID varchar(20) primary key,
-    methodType varchar(20));
-    
 create table services(
-	serviceID varchar(20) primary key,
-	serviceType varchar(20),
-	computeID varchar(20),
+    serviceID varchar(20) primary key,
+    serviceType varchar(20),
+    computeID varchar(20),
     storageID varchar(20),
     subscriptionID varchar(20),
     foreign key (subscriptionID) references subscription (subscriptionID) on delete cascade,
     foreign key (storageID) references storage (storageID) on delete cascade,
     foreign key (computeID) references computing (computeID)on delete cascade);
-        
-create table computing(
-	computeID varchar(20) primary key,
-	coreCount varchar(20),
-	ram varchar(20),
-	gpu varchar(20));
-	
+    
 create table instance(
-	instanceID varchar(20) primary key,
+    instanceID varchar(20) primary key,
     zone varchar(20),
     instanceType varchar(20),
     allocated bool,
     computeID varchar(20),
     storageID varchar(20),
-	foreign key (storageID) references storage (storageID) on delete cascade,
+    foreign key (storageID) references storage (storageID) on delete cascade,
     foreign key (computeID) references computing (computeID)on delete cascade);
     
-    
-create table storage(
-	storageID varchar(20) primary key,
-    size varchar(20),
-    type varchar(20));
-    
 create table logs(
-	logID varchar(20) primary key,
+    logID varchar(20) primary key,
     description varchar(20),
     timestamp timestamp,
     instanceID varchar(20),
-	foreign key (instanceID) references instance (instanceID) on delete cascade);
+    foreign key (instanceID) references instance (instanceID) on delete cascade);
     
-			
+            
 
